9e2202e99f8001fda98aa516c47a25c7db0ba430
Giselle H <giselleh@Giselle-H-MacBook-Pro.local>
2019-07-10 13:30:43 -0700

    Intial change

---
+++ javascript.js
@@ -0,0 +1,10 @@
+alert("Hello! I am an alert box!");
+
+var colors = ["white","pink","violet","aqua","lavender","plum"]
+math.random() = colors
+function backgroundChange() {
+  var index = Math.floor(Math.random() * 4);
+  document.body.style.backgroundColor = colors[index];
+
+
+}
